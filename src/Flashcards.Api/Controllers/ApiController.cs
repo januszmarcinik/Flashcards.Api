e2f7cd7e28c5bde27486f676605fc1b152a8756d
Janusz Marcinik <januszmarcinik@gmail.com>
2018-03-24 22:15:29 +0100

    Add API project

---
+++ src/Flashcards.Api/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flashcards.Api.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        [HttpGet]
+        public string Get()
+            => "Flashcards works!";
+    }
+}
