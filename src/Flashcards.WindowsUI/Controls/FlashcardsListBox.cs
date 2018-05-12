1928fedd0775b87cb24f281762273bb681d2f607
Janusz Marcinik <januszmarcinik@gmail.com>
2018-05-12 14:48:01 +0200

    Exploring resources

---
+++ src/Flashcards.WindowsUI/Controls/FlashcardsListBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Flashcards.WindowsUI.Infrastructure;
+
+namespace Flashcards.WindowsUI.Controls
+{
+    public class FlashcardsListBox : ListBox
+    {
+        public void LoadItems(IEnumerable<IControlItem> items)
+        {
+            Items.Clear();
+            Items.AddRange(items.ToArray());
+            DisplayMember = nameof(IControlItem.Display);
+        }
+    }
+}
--- src/Flashcards.WindowsUI/Forms/ResourcesExplorer/ResourcesExplorerForm.cs
+++ src/Flashcards.WindowsUI/Forms/ResourcesExplorer/ResourcesExplorerForm.cs
@@ -1,20 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Flashcards.WindowsUI.Controls;
+using Flashcards.WindowsUI.Models;
+using Flashcards.WindowsUI.Services;
 
 namespace Flashcards.WindowsUI.Forms.ResourcesExplorer
 {
-    public partial class ResourcesExplorerForm : Form
+    public partial class ResourcesExplorerForm : FlashcardsForm
     {
-        public ResourcesExplorerForm()
+        private readonly CategoriesService _categoriesService;
+        private readonly DecksService _decksService;
+        private readonly CardsService _cardsService;
+
+        private readonly Topic _topic;
+
+        public ResourcesExplorerForm(Topic topic)
         {
             InitializeComponent();
+            _topic = topic;
+
+            _categoriesService = new CategoriesService();
+            _decksService = new DecksService();
+            _cardsService = new CardsService();
+
+            RefreshCategories();
+        }
+
+        private void RefreshCategories()
+        {
+            lbCategories.LoadItems(_categoriesService.GetAll(_topic));
+        }
+
+        private void RefreshDecks(string category)
+        {
+            lbDecks.LoadItems(_decksService.GetAll(_topic, category));
+        }
+
+        private void RefreshCards(string category, string deck)
+        {
+            lbCards.LoadItems(_cardsService.GetAll(_topic, category, deck));
+        }
+
+        private void lbCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbCategories.SelectedItem != null)
+            {
+                RefreshDecks((lbCategories.SelectedItem as Category)?.Name);
+                lbCards.Items.Clear();
+            }
+        }
+
+        private void lbDecks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbCategories.SelectedItem != null && lbDecks.SelectedItem != null)
+            {
+                RefreshCards((lbCategories.SelectedItem as Category)?.Name, (lbDecks.SelectedItem as Deck)?.Name);
+            }
         }
     }
 }
